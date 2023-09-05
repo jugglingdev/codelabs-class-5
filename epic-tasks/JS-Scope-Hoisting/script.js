2d21974552d9c0a9e23644ed401f43ccf6befdf4
jugglingdev <kaylamarie1785@gmail.com>
2023-09-04 22:38:29 -0500

    Demonstrated hoisting

--- epic-tasks/JS-Scope-Hoisting/script.js
+++ epic-tasks/JS-Scope-Hoisting/script.js
@@ -20,3 +20,45 @@ myName();
 
 console.log(name);  // Chris
 
+
+/* Exercise 2 */
+
+/* *** Var Test *** */
+
+function hoistingTest(sentence) {
+    console.log(sentence);
+}
+
+// hoistingTest(sentence);  // undefined
+
+var sentence = 'I wanna dance with somebody';
+
+hoistingTest(sentence);  // I wanna dance with somebody
+
+/* *** Let Test *** */
+
+function hoistingTest(sentence2) {
+    console.log(sentence2);
+}
+
+// hoistingTest(sentence2);  // Cannot access 'sentence2' before initialization
+
+let sentence2 = 'I wanna sing with somebody';
+
+hoistingTest(sentence2);  // I wanna sing with somebody
+
+/* *** Const Test *** */
+
+function hoistingTest(sentence3) {
+    console.log(sentence3);
+}
+
+// hoistingTest(sentence3);  // Cannot access 'sentence3' before initialization
+
+const sentence3 = 'I wanna laugh with somebody';
+
+hoistingTest(sentence3);  // I wanna laugh with somebody
+
+
+/* Exercise 3 */
+
