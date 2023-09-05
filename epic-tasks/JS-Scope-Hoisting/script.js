7bcdfe4555754f6a8c609c15a82ef4950b9d03d6
jugglingdev <kaylamarie1785@gmail.com>
2023-09-04 22:26:56 -0500

    Demonstrated variable scope

---
+++ epic-tasks/JS-Scope-Hoisting/script.js
@@ -0,0 +1,22 @@
+// EXERCISES
+
+/* Exercise 1 */
+
+let name = 'Chris';
+
+console.log(name);  // Chris
+
+if (true) {
+    let name = 'Alex';
+    console.log(name);  // Alex
+}
+
+function myName() {
+    let name = 'Kayla';
+    console.log(name);  // Kayla
+}
+
+myName();
+
+console.log(name);  // Chris
+
