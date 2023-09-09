4f62797142fc97ca00ca749b3fe0990445d4cc1b
jugglingdev <kaylamarie1785@gmail.com>
2023-09-08 20:55:41 -0500

    Complete Question 2: Single Number

--- in-class-exercises/interview-practice.js
+++ in-class-exercises/interview-practice.js
@@ -2,54 +2,72 @@
 
 // Question #1: Array Sum
 
-function arraySum(nums, target) {
-    let sum = 0;
+console.groupCollapsed('1: Array Sum');
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            sum = nums[i] + nums[j];
+    function arraySum(nums, target) {
+        let sum = 0;
 
-            if (i != j && sum == target) {
-                console.log([i, j]);
-                return;
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                sum = nums[i] + nums[j];
+
+                if (i != j && sum == target) {
+                    console.log([i, j]);
+                    return;
+                }
             }
         }
     }
-}
 
-arraySum([2, 7, 11, 15], 9); // [0, 1]
+    arraySum([2, 7, 11, 15], 9); // [0, 1]
+
+console.groupEnd();
 
 // Question #2: Single Number
 
-function singleNumber(nums) {
-    let countStack = [];
+console.groupCollapsed('2: Single Number');
 
-    for (let i = 0; i < nums.length; i++) {
+    function singleNumber(nums) {
+        let singleNumber = [];
 
+        for (let num of nums) {
+            if (!singleNumber.includes(num)) {
+                singleNumber.push(num);
+            } else {
+                // Remove the number if it's already in singleNumber[]
+                singleNumber.splice(singleNumber.indexOf(num), 1);
+            }
+        }
+
+        console.log(singleNumber[0]);
     }
-}
 
-singleNumber([4, 1, 2, 1, 2]);  // 4
+    singleNumber([4, 1, 2, 1, 2]);  // 4
+    singleNumber([1, 3, 5, 3, 1]);  // 5
 
-
+console.groupEnd();
 
 // Question #3: FizzBuzz
 
-function FizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log('FizzBuzz');
-        } else if (i % 5 == 0) {
-            console.log('Fizz');
-        } else if (i % 3 == 0) {
-            console.log('Buzz');
-        } else {
-            console.log(i.toString());
+console.groupCollapsed('3: FizzBuzz');
+
+    function FizzBuzz(n) {
+        for (let i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                console.log('FizzBuzz');
+            } else if (i % 5 == 0) {
+                console.log('Fizz');
+            } else if (i % 3 == 0) {
+                console.log('Buzz');
+            } else {
+                console.log(i.toString());
+            }
         }
     }
-}
 
-FizzBuzz(20);
+    FizzBuzz(20);
+
+console.groupEnd();
 
 // Question #4: First Unique Character
 
